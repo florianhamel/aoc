4bcd59db37b2027b961f9452c55f932201de4edf
Florian Hamel <fhamel.freelance@gmail.com>
2025-12-06 11:08:44 +0100

    ✨ day03 - 2/2

--- aoc-2025/day03/day03.test.ts
+++ aoc-2025/day03/day03.test.ts
@@ -1,39 +1,42 @@
 import { fromAoc2025, getFileContent } from '../../shared/utils';
-import { getJoltage, sumJoltages } from './day03';
+import { getSpecificJoltage as getSpecificVoltage, sumJoltages } from './day03';
 
 describe('day03', () => {
   describe('day03 - part 1/2', () => {
-    it.each([
-      { bank: '987654321111111', expected: '98' },
-      { bank: '811111111111119', expected: '89' },
-      { bank: '234234234234278', expected: '78' },
-      { bank: '818181911112111', expected: '92' },
-    ])('should joltage be $expected for bank $bank', (it) => {
-      const joltage = getJoltage(it.bank);
-      expect(joltage).toBe(it.expected);
-    });
-
-    it('should sum joltages', () => {
-      const banks = ['987654321111111', '811111111111119', '234234234234278', '818181911112111'];
-      const joltage = sumJoltages(banks);
-      expect(joltage).toBe('357');
-    });
-  });
-
-  describe('day03 - part 2/2', () => {
     it.each([
       { bank: '987654321111111', expected: 98 },
       { bank: '811111111111119', expected: 89 },
       { bank: '234234234234278', expected: 78 },
       { bank: '818181911112111', expected: 92 },
-    ])('should solve part 2', (it) => {});
+    ])('should joltage be $expected for bank $bank', (it) => {
+      const joltage = getSpecificVoltage(it.bank, 2);
+      expect(joltage).toBe(it.expected);
+    });
+
+    it('should sum joltages', () => {
+      const banks = ['987654321111111', '811111111111119', '234234234234278', '818181911112111'];
+      const joltage = sumJoltages(banks, 2);
+      expect(joltage).toBe(357);
+    });
+  });
+
+  describe('day03 - part 2/2', () => {
+    it.each([
+      { bank: '987654321111111', expected: 987654321111 },
+      { bank: '811111111111119', expected: 811111111119 },
+      { bank: '234234234234278', expected: 434234234278 },
+      { bank: '818181911112111', expected: 888911112111 },
+    ])('should solve part 2', (it) => {
+      const joltage = getSpecificVoltage(it.bank, 12);
+      expect(joltage).toBe(it.expected);
+    });
   });
 
   it('should log answers', async () => {
     const raw = await getFileContent(fromAoc2025('day03'));
     const banks = raw.split('\n');
 
-    console.log('Part 1:', sumJoltages(banks));
-    // console.log('Part 2:' /* result */);
+    console.log('Part 1:', sumJoltages(banks, 2));
+    console.log('Part 2:', sumJoltages(banks, 12));
   });
 });
--- aoc-2025/day03/day03.ts
+++ aoc-2025/day03/day03.ts
@@ -1,12 +1,35 @@
-export function sumJoltages(banks: Readonly<string[]>) {
+export function getSpecificJoltage(bank: string, batteries: number) {
+  let output = '';
+  let maxIndex = 0;
+  let maxValue = 0;
+  while (batteries > 0) {
+    for (let i = maxIndex; i < bank.length - batteries + 1; i++) {
+      const curr = +bank[i]!
+      if (curr > maxValue) {
+        maxIndex = i;
+        maxValue = curr;
+      }
+    }
+    output += maxValue;
+    maxIndex++;
+    maxValue = 0;
+    batteries--;
+  }
+  return +output;
+}
+
+export function sumJoltages(banks: Readonly<string[]>, batteries: number) {
   let sum = 0;
   for (let bank of banks) {
-    sum += +getJoltage(bank);
+    sum += getSpecificJoltage(bank, batteries);
   }
   return sum;
 }
 
-export function getJoltage(bank: Readonly<string>) {
+/**
+ * @deprecated
+ */
+function getJoltage(bank: string) {
   let output = '';
   let maxValue = 0;
   let maxIndex = 0;
